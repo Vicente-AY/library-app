20f837bb0c6140f0ac6d0eb26747d8d17583871b
Vicente Alberola <vicalbyag@alu.edu.gva.es>
2026-08-12 12:32:25 +0200

    Creado menu principal primigenio

--- Menus/MainMenu.cs
+++ Menus/MainMenu.cs
@@ -2,5 +2,38 @@ namespace Menus;
 
 public class MainMenu
 {
-    
+    public void MainMenuOptions()
+    {
+
+        bool iterate = true;
+
+        Console.WriteLine("Welcome to Library-App");
+
+        while(iterate){
+            Console.WriteLine("Please select an option");
+            Console.WriteLine("1. Login | 2. Register | 3. Exit");
+
+            string input = Console.ReadLine();
+
+            //excepciones
+
+            int option = int.Parse(input);
+
+            switch (option)
+            {
+                case 1: 
+                    //login
+                    break;
+                case 2:
+                    //register
+                    break;
+                case 3:
+                    Console.WriteLine("Bye!");
+                    iterate = false;
+                    break;
+            }
+        }
+
+        return;
+    }
 }
