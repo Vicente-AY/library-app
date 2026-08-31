3201d4deaf16dd98f60ee368af4efe8fa662181c
Vicente Alberola <vicalbyag@alu.edu.gva.es>
2026-08-31 12:30:06 +0200

    Crada ventana de loan y la clase boolToCardBackgroundConverter para cambiar el fondo de la tarjeta cuando un item es seleccionado

---
+++ GUI/Loans/BoolToCardBackgroundConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace library_app.GUI.Loans
+{
+    public class BoolToCardBackgroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool selected = value is bool b && b;
+            return selected ? new SolidColorBrush(Color.FromRgb(220, 235, 255)) : Brushes.White;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
---
+++ GUI/Loans/LoanWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace library_app.GUI.Loans
+{
+    /// <summary>
+    /// Lógica de interacción para LoanWindow.xaml
+    /// </summary>
+    public partial class LoanWindow : Window
+    {
+        public LoanWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
