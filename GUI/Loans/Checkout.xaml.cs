078e3d5ca0c172425f10b9cd7834f79a635b9b3e
Vicente Alberola <vicalbyag@alu.edu.gva.es>
2026-09-02 18:17:47 +0200

    Creado el code behind de la ventana de checkout

--- GUI/Loans/Checkout.xaml.cs
+++ GUI/Loans/Checkout.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Items;
+using library_app.GUI.GuiMenu;
+using Loans;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,7 +13,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using Items;
 
 namespace library_app.GUI.Loans
 {
@@ -19,28 +22,70 @@ namespace library_app.GUI.Loans
     public partial class Checkout : Window
     {
 
-        List<LibraryItem> items = new List<LibraryItem>();
+        private ObservableCollection<LibraryItem> items;
 
         public Checkout(List<LibraryItem> selectedItems)
         {
             InitializeComponent();
 
-            this.items = selectedItems;
+            this.items = new ObservableCollection<LibraryItem>(selectedItems);
+            lvCheckoutItems.ItemsSource = items;
         }
 
         private void BtnRemoveItem_Click(object sender, RoutedEventArgs e)
         {
+            if (sender is Button btn && btn.Tag is LibraryItem itemToRemove)
+            {
+                items.Remove(itemToRemove);
 
+                if (items.Count == 0)
+                {
+                    var result = MessageBox.Show("You have removed all the Selected Items",
+                                 "Confirm", MessageBoxButton.RetryCancel, MessageBoxImage.Warning);
+
+                    if (result == MessageBoxResult.Retry)
+                    {
+                        LoanWindow lWindow = new LoanWindow();
+                        lWindow.Show();
+                    }
+                    if (result == MessageBoxResult.Cancel)
+                    {
+                        UserMenu uMenu = new UserMenu();
+                        uMenu.Show();
+                    }
+
+                    this.Close();
+
+                }
+            }
         }
 
         private void BtnCancelLoan_Click(object sender, RoutedEventArgs e)
         {
+            if(items.Count > 0)
+            {
+                var result = MessageBox.Show("You have items pending. Do you want to Cancel?", 
+                             "Confirm", MessageBoxButton.YesNo, MessageBoxImage.Warning);
 
+                if(result == MessageBoxResult.Yes)
+                {
+                    items.Clear();
+
+                    UserMenu uMenu = new UserMenu();
+                    uMenu.Show();
+
+                    this.Close();
+                }
+                else
+                {
+                    return;
+                }
+            }
         }
 
         public void BtnConfirmLoan_Click(object sender, RoutedEventArgs e)
         {
-
+            //Hacer logica de cerar loans
         }
 
     }
