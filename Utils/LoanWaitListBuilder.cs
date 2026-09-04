1c9db9af278b7e3dd141f40cd81995e1eb096ed4
Vicente Alberola <vicalbyag@alu.edu.gva.es>
2026-09-04 13:25:07 +0200

    Refactorizada clase que crea Loans para hacerla mas util en el contexto GUI actual

--- Utils/LoanWaitListBuilder.cs
+++ Utils/LoanWaitListBuilder.cs
@@ -8,23 +8,29 @@ namespace Utils;
 
 public class LoanWaitlistBuilder
 {
-    public void LoanCreation(User user, LibraryItem item, LibraryContext db)
+    public void LoanCreation(User user, List<LibraryItem> items)
     {
-
-        int id = 0;
-        DateTime loanCreated = DateTime.Now;
-        int days = (item is Book) ? 15 : 7;
-        DateTime expectedReturn = loanCreated.AddDays(days);
-
-        id = (db.Loans.Select(i => (int?)i.id).Max() ?? 0) + 1 ;
-
-        Loan loan = new Loan(id, item, loanCreated, expectedReturn, user);
-
-        db.Loans.Add(loan);
-        user.loanList.Add(loan);
         NotificacionGenerator notGen = new NotificacionGenerator();
-        notGen.GenerateNotification(user, $"Successfuly loaned ID: {item.id} | {item.title}. Return Date: {expectedReturn.ToString("dd/MM/yyyy")}");
-        item.availability = Availability.Lent;
+        using (var db = new LibraryContext())
+        {
+            foreach (var item in items)
+            {
+                int id = 0;
+                DateTime loanCreated = DateTime.Now;
+                int days = (item is Book) ? 15 : 7;
+                DateTime expectedReturn = loanCreated.AddDays(days);
+
+                id = (db.Loans.Select(i => (int?)i.id).Max() ?? 0) + 1;
+
+                Loan loan = new Loan(id, item, loanCreated, expectedReturn, user);
+
+                db.Loans.Add(loan);
+                user.loanList.Add(loan);
+                notGen.GenerateNotification(user, $"Successfuly loaned ID: {item.id} | {item.title}. Return Date: {expectedReturn.ToString("dd/MM/yyyy")}");
+                item.availability = Availability.Lent;
+            }
+            db.SaveChanges();
+        }
     }
 
     public WaitEntry WaitListCreation(User user, LibraryItem item)
